487bdf3aa69a66aa8057f00ca918c0a66b864fe1
GotSteven <gotsteve0812@gmail.com>
2023-06-16 06:34:52 +0900

    マインスイーパー初回

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,179 @@
+import type { MouseEvent } from 'react';
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const bombCount = 10;
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  );
+  const board: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  const directions = [
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+  ];
+  const checkAround = (x: number, y: number) => {
+    let bombs = 0;
+    for (const [dy, dx] of directions) {
+      if (
+        bombMap[y + dy] !== undefined &&
+        bombMap[y + dy][x + dx] !== undefined &&
+        bombMap[y + dy][x + dx] === 1
+      ) {
+        bombs++;
+      }
+    }
+    board[y][x] = bombs;
+    if (bombs === 0) {
+      for (const [dy, dx] of directions) {
+        if (
+          board[y + dy] !== undefined &&
+          board[y + dy][x + dx] !== undefined &&
+          (board[y + dy][x + dx] === -1 ||
+            board[y + dy][x + dx] === 9 ||
+            board[y + dy][x + dx] === 10)
+        ) {
+          checkAround(x + dx, y + dy);
+        }
+      }
+    }
+  };
+  const makeBoard = () => {
+    outer: for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (userInputs[y][x] === 0) {
+          board[y][x] = -1;
+        } else if (userInputs[y][x] === 1) {
+          checkAround(x, y);
+          break outer;
+        } else if (userInputs[y][x] === 2) {
+          board[y][x] = 9;
+        } else if (userInputs[y][x] === 3) {
+          board[y][x] = 10;
+        }
+      }
+    }
+  };
+  makeBoard();
+
+  const onRightClick = (x: number, y: number, event: MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+    switch (userInputs[y][x]) {
+      case 0:
+        newUserInputs[y][x] = 2;
+        board[y][x] = 9;
+        break;
+      case 2:
+        newUserInputs[y][x] = 3;
+        board[y][x] = 10;
+        break;
+      case 3:
+        newUserInputs[y][x] = 0;
+        board[y][x] = -1;
+        break;
+    }
+    setUserInputs(newUserInputs);
+    makeBoard();
+  };
+  const onLeftClick = (x: number, y: number) => {
+    if (board[y][x] === -1) {
+      const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+      newUserInputs[y][x] = 1;
+      setUserInputs(newUserInputs);
+      makeBoard();
+    }
+  };
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((state, x) => (
+            <div
+              className={styles.cell}
+              key={`${x}-${y}`}
+              onContextMenu={(event) => onRightClick(x, y, event)}
+              onClick={() => onLeftClick(x, y)}
+            >
+              {state === -1 && <div className={styles.stone} />}
+              {state === 1 && <div className={styles.icon} />}
+              {state === 2 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-30px 0' }} />
+              )}
+              {state === 3 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-60px 0' }} />
+              )}
+              {state === 4 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-90px 0' }} />
+              )}
+              {state === 5 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-120px 0' }} />
+              )}
+              {state === 6 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-150px 0' }} />
+              )}
+              {state === 7 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-180px 0' }} />
+              )}
+              {state === 8 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-210px 0' }} />
+              )}
+              {state === 9 && (
+                <div className={styles.stone}>
+                  <div className={styles.icon} style={{ backgroundPosition: '-240px 0' }} />
+                </div>
+              )}
+              {state === 10 && (
+                <div className={styles.stone}>
+                  <div className={styles.icon} style={{ backgroundPosition: '-270px 0' }} />
+                </div>
+              )}
+              {state === 11 && (
+                <div className={styles.icon} style={{ backgroundPosition: '-300px 0' }} />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
