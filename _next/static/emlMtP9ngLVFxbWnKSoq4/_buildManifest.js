f2108fe6a76254a384e5d9f39ea3812db01f9754
GotSteven <GotSteven@users.noreply.github.com>
2023-06-15 21:36:27 +0000

    deploy: 487bdf3aa69a66aa8057f00ca918c0a66b864fe1

---
+++ _next/static/emlMtP9ngLVFxbWnKSoq4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7acd0e3dbddd1dd7.css","static/chunks/pages/index-768b56b74cc776a3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
