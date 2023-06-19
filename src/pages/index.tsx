c13eca0835156de6471659d1b6903f8367185895
GotSteven <s1f102300481@iniad.org>
2023-06-19 21:20:07 +0900

    ほぼ完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,13 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+  // [userInputs]
+  // | 0 = 未クリック
+  // | 1 = 左クリック
+  // | 2 = 旗
+  // | 3 = はてな
+
   const bombCount = 10;
   const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,21 +33,47 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const newBombMap: number[][] = JSON.parse(JSON.stringify(bombMap));
+  // [bombMap]
+  // | 0 = ボムなし
+  // | 1 = ボムあり
+
+  const board: number[][] = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ];
+  // [board]
+  // | -1 = 石
+  // | 0 = 石なし表示なし
+  // | 1~8 = 数字表示
+  // | 9 = 石 + はてな
+  // | 10 = 石 + 旗
+  // | 11 = 爆弾
+
+  let gameState = 0;
+  // [GameState]
+  // | 0 : スタート前
+  // | 1 : ゲーム中
+  // | 2 : ゲーム終了(クリア)
+  // | 3 : ゲーム終了(爆発)
+
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  if (isPlaying) gameState = 1;
+
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
-  const board: number[][] = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  if (isFailure) {
+    gameState = 3;
+    console.log('爆破');
+  }
   const directions = [
     [-1, 0],
     [-1, -1],
@@ -51,24 +84,34 @@ const Home = () => {
     [0, 1],
     [-1, 1],
   ];
-  function getRandomInt(min: number, max: number) {
+  const checkClear = () => {
+    let stoneCount = 0;
+    for (let x = 0; x < 9; x++) {
+      for (let y = 0; y < 9; y++) {
+        if (board[y][x] === -1 || board[y][x] === 9 || board[y][x] === 10) stoneCount++;
+      }
+    }
+    if (stoneCount === 10) {
+      gameState = 2;
+      console.log('クリア');
+    }
+  };
+  const getRandomInt = (min: number, max: number) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
-  }
-  const setBomb = () => {
-    const newBombMap: number[][] = JSON.parse(JSON.stringify(bombMap));
-    for (let i = 0; i <= bombCount; ) {
-      const r1 = getRandomInt(0, 8);
-      const r2 = getRandomInt(0, 8);
-      if (bombMap[r1][r2] === 0) {
-        newBombMap[r1][r2] = 1;
-        i++;
-      }
-    }
-    setBombMap(newBombMap);
   };
-  setBomb();
+  const setBomb = (x: number, y: number) => {
+    const r1 = getRandomInt(0, 9);
+    const r2 = getRandomInt(0, 9);
+    if (r1 === x && r2 === y) {
+      setBomb(x, y);
+    } else if (newBombMap[r1][r2] === 0) {
+      newBombMap[r1][r2] = 1;
+    } else {
+      setBomb(x, y);
+    }
+  };
   const checkAround = (x: number, y: number) => {
     let bombs = 0;
     for (const [dy, dx] of directions) {
@@ -95,30 +138,9 @@ const Home = () => {
       }
     }
   };
-  const makeBoard = () => {
-    outer: for (let y = 0; y <= 8; y++) {
-      for (let x = 0; x <= 8; x++) {
-        if (bombMap[y][x] === 1) {
-          board[y][x] = 11;
-        }
-        if (userInputs[y][x] === 0) {
-          board[y][x] = -1;
-        } else if (userInputs[y][x] === 1) {
-          checkAround(x, y);
-          break outer;
-        } else if (userInputs[y][x] === 2) {
-          board[y][x] = 9;
-        } else if (userInputs[y][x] === 3) {
-          board[y][x] = 10;
-        }
-      }
-    }
-  };
-  makeBoard();
 
   const onRightClick = (x: number, y: number, event: MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
     switch (userInputs[y][x]) {
       case 0:
         newUserInputs[y][x] = 2;
@@ -134,18 +156,55 @@ const Home = () => {
         break;
     }
     setUserInputs(newUserInputs);
-    makeBoard();
   };
   const onLeftClick = (x: number, y: number) => {
-    if (board[y][x] === -1) {
-      const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
-      newUserInputs[y][x] = 1;
-      setUserInputs(newUserInputs);
-      makeBoard();
+    if (gameState <= 1) {
+      if (!isPlaying) {
+        for (let i = 0; i < bombCount; i++) {
+          setBomb(x, y);
+        }
+        setBombMap(newBombMap);
+      }
+      if (userInputs[y][x] === 0) {
+        newUserInputs[y][x] = 1;
+        setUserInputs(newUserInputs);
+      }
     }
   };
+  const makeBoard = () => {
+    for (let x = 0; x < 9; x++) {
+      for (let y = 0; y < 9; y++) {
+        if (userInputs[x][y] === 1) {
+          if (bombMap[x][y] === 0) {
+            checkAround(y, x);
+          } else {
+            board[x][y] = 11;
+          }
+        } else if (userInputs[x][y] === 2) {
+          board[x][y] = 9;
+        } else if (userInputs[x][y] === 3) {
+          board[x][y] = 10;
+        } else if (isFailure && bombMap[x][y] === 1) {
+          board[x][y] = 11;
+        }
+      }
+    }
+  };
+  makeBoard();
+  checkClear();
   return (
     <div className={styles.container}>
+      <div className={styles.header}>
+        {gameState <= 1 && (
+          <div className={styles.icon} style={{ backgroundPosition: '-330px 0' }} />
+        )}
+        {gameState === 2 && (
+          <div className={styles.icon} style={{ backgroundPosition: '-360px 0' }} />
+        )}
+        {gameState === 3 && (
+          <div className={styles.icon} style={{ backgroundPosition: '-390px 0' }} />
+        )}
+      </div>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((state, x) => (
@@ -178,12 +237,12 @@ const Home = () => {
               {state === 8 && (
                 <div className={styles.icon} style={{ backgroundPosition: '-210px 0' }} />
               )}
-              {state === 9 && (
+              {state === 10 && (
                 <div className={styles.stone}>
                   <div className={styles.icon} style={{ backgroundPosition: '-240px 0' }} />
                 </div>
               )}
-              {state === 10 && (
+              {state === 9 && (
                 <div className={styles.stone}>
                   <div className={styles.icon} style={{ backgroundPosition: '-270px 0' }} />
                 </div>
